58fcd2c52a534041de9b90f8c3551b1c1ee0f3ee
suittizihou <suittizihou@gmail.com>
2021-04-10 20:01:52 +0900

    GameJobsにあるAddGameObjectからも生成時にタグ、名前を決められるよう引数を追加した

--- EvaEngine/EvaEngine/GameSystemBase/Base/GameJobs/GameJobs.cpp
+++ EvaEngine/EvaEngine/GameSystemBase/Base/GameJobs/GameJobs.cpp
@@ -3,9 +3,9 @@
 
 using namespace EvaEngine;
 
-std::weak_ptr<GameObject> GameJobs::AddGameObject()
+std::weak_ptr<GameObject> GameJobs::AddGameObject(const std::string& tag, const std::string& name)
 {
-    return GameObjectManager::Instance().AddGameObject(m_SceneName, "None", "GameObject");
+    return GameObjectManager::Instance().AddGameObject(m_SceneName, tag, name);
 }
 
 void GameJobs::SetSceneName(const std::string& sceneName)
