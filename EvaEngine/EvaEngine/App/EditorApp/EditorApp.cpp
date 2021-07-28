d7bdf0edde7f5287e9a24129a4408e751359f06b
suittizihou <suittizihou@gmail.com>
2021-07-28 16:35:59 +0900

    一部不要なフォルダを削除

--- EvaEngine/EvaEngine/App/EditorApp/EditorApp.cpp
+++ EvaEngine/EvaEngine/App/EditorApp/EditorApp.cpp
@@ -19,8 +19,8 @@
 #include "../../Editor/EditorWindows/DemoWindow/DemoWindow.h"
 
 #include <imgui.h>
-#include <imgui_impl_win32.h>
-#include <imgui_impl_dx11.h>
+#include <backends/imgui_impl_win32.h>
+#include <backends/imgui_impl_dx11.h>
 #include <ImGuizmo.h>
 #include <iostream>
 
--- EvaEngine/EvaEngine/Editor/EditorCommand/EditorCommand.cpp
+++ EvaEngine/EvaEngine/Editor/EditorCommand/EditorCommand.cpp
@@ -1,7 +1,7 @@
 #if _DEBUG
 #include <imgui.h>
-#include <imgui_impl_win32.h>
-#include <imgui_impl_dx11.h>
+#include <backends/imgui_impl_win32.h>
+#include <backends/imgui_impl_dx11.h>
 #include <ImGuizmo.h>
 
 #include "EditorCommand.h"
